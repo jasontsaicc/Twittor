9184b4802dd87928856bc53265b61f184544e195
jason tsai <jason.tsaicc@gmail.com>
2021-08-10 18:10:00 +0800

    0.2 Signed-off-by: jason tsai <jason.tsaicc@gmail.com>

---
+++ twittor/route.py
@@ -0,0 +1,29 @@
+from flask import render_template, redirect, url_for
+from twittor.forms import LoginForm
+
+def index():
+    name = {'username':'root'}
+    posts = [
+        {
+            'author':{'username':'root'},
+            'body':"hi i'm roots!"
+        }, {
+            'author':{'username':'test'},
+            'body':"hi i'm test!"
+        },
+    ]
+    return render_template('index.html',name=name,posts=posts
+                           )
+
+
+def login():
+    form = LoginForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        msg = "username={}, password={}, remember_me={}".format(
+            form.username.data,
+            form.password.data,
+            form.remember_me.data,
+        )
+        print(msg)
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sign In", form=form )
