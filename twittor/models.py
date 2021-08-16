dda014ebf423bb38c24055ef832815aedbfac9e4
jason tsai <jason.tsaicc@gmail.com>
2021-08-17 00:38:08 +0800

    version 0.4
    Signed-off-by: jason tsai <jason.tsaicc@gmail.com>

--- twittor/models.py
+++ twittor/models.py
@@ -1,7 +1,9 @@
-from twittor import db
+from twittor import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from datetime import datetime
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
@@ -15,6 +17,17 @@ class User(db.Model):
             self.email, 
             self.password_hash
             )
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
