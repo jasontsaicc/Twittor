ba5133d45ccd765ec76af578ab35b99c416f1bf9
jason.tsai <jason.tsaicc@gmail.com>
2021-08-31 00:42:45 +0800

    version 0.9
    Signed-off-by: jason.tsai <jason.tsaicc@gmail.com>

--- twittor/route.py
+++ twittor/route.py
@@ -1,8 +1,11 @@
-from flask import render_template, redirect, url_for, request, abort, current_app
+from flask import render_template, redirect, url_for, request, \
+    abort, current_app, flash
 from flask_login import login_user, current_user, logout_user, login_required
-from twittor.forms import LoginForm, RegisterForm, EditProfileForm, TweetForm
+from twittor.forms import LoginForm, RegisterForm, EditProfileForm, TweetForm, \
+    PasswdResetRequestForm, PasswdResetForm
 from twittor.models import User, Tweet, load_user, Tweet
 from twittor import db
+from twittor.email import send_email
 
 @login_required
 def index():
@@ -15,13 +18,12 @@ def index():
     page_num = int(request.args.get('page') or 1)
     tweets = current_user.own_and_followed_tweets().paginate(
         page=page_num, per_page=current_app.config['TWEET_PER_PAGE'], error_out=False)
-    
+
     next_url = url_for('index', page=tweets.next_num) if tweets.has_next else None
-    # next_url = url_for('index', page=tweets.next_num)
     prev_url = url_for('index', page=tweets.prev_num) if tweets.has_prev else None
-    
     return render_template(
-        'index.html', tweets=tweets.items, form=form, next_url=next_url, prev_url=prev_url)
+        'index.html', tweets=tweets.items, form=form, next_url=next_url, prev_url=prev_url
+    )
 
 
 def login():
@@ -57,24 +59,25 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Registration', form=form)
 
+@login_required
 def user(username):
     u = User.query.filter_by(username=username).first()
     if u is None:
         abort(404)
     page_num = int(request.args.get('page') or 1)
     tweets = u.tweets.order_by(Tweet.create_time.desc()).paginate(
-        page=page_num, 
-        per_page=current_app.config['TWEET_PER_PAGE'], 
+        page=page_num,
+        per_page=current_app.config['TWEET_PER_PAGE'],
         error_out=False)
-    
-    next_url = url_for('profile', 
-        page=tweets.next_num, 
-        username=username) if tweets.has_next else None
-    # next_url = url_for('index', page=tweets.next_num)
-    prev_url = url_for('profile', 
-        page=tweets.prev_num,  
-        username=username) if tweets.has_prev else None
 
+    next_url = url_for(
+        'profile',
+        page=tweets.next_num,
+        username=username) if tweets.has_next else None
+    prev_url = url_for(
+        'profile',
+        page=tweets.prev_num,
+        username=username) if tweets.has_prev else None
     if request.method == 'POST':
         if request.form['request_button'] == 'Follow':
             current_user.follow(u)
@@ -82,12 +85,19 @@ def user(username):
         else:
             current_user.unfollow(u)
             db.session.commit()
-    return render_template('user.html', title='Profile', tweets=tweets.items, user=u, 
-        next_url=next_url, 
-        prev_url=prev_url)
+    return render_template(
+        'user.html',
+        title='Profile',
+        tweets=tweets.items,
+        user=u,
+        next_url=next_url,
+        prev_url=prev_url
+    )
+
 
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @login_required
 def edit_profile():
@@ -99,3 +109,59 @@ def edit_profile():
         db.session.commit()
         return redirect(url_for('profile', username=current_user.username))
     return render_template('edit_profile.html', form=form)
+
+
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = PasswdResetRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            flash(
+                "You should soon receive an email allowing you to reset your \
+                password. Please make sure to check your spam and trash \
+                if you can't find the email."
+            )
+            token = user.get_jwt()
+            url_password_reset = url_for(
+                'password_reset',
+                token=token,
+                _external=True
+            )
+            url_password_reset_request = url_for(
+                'reset_password_request',
+                _external=True
+            )
+            send_email(
+                subject=current_app.config['MAIL_SUBJECT_RESET_PASSWORD'],
+                recipients=[user.email],
+                text_body= render_template(
+                    'email/passwd_reset.txt',
+                    url_password_reset=url_password_reset,
+                    url_password_reset_request=url_password_reset_request
+                ),
+                html_body=render_template(
+                    'email/passwd_reset.html',
+                    url_password_reset=url_password_reset,
+                    url_password_reset_request=url_password_reset_request
+                )
+            )
+        return redirect(url_for('login'))
+    return render_template('password_reset_request.html', form=form)
+
+
+def password_reset(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_jwt(token)
+    if not user:
+        return redirect(url_for('login'))
+    form = PasswdResetForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template(
+        'password_reset.html', title='Password Reset', form=form
+    )
